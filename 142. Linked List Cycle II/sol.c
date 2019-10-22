e980d7ab5f24dee186c3a68c1c00997824741ee3
demonic <ian910297@gmail.com>
2019-10-22 08:19:28 +0800

    Add .gitignore and solve the 142 linked list cycle question
    Add Python and C++ .gitignore
    Solve the question via a special technique, which use address as marked
    label

---
+++ 142. Linked List Cycle II/sol.c
@@ -0,0 +1,71 @@
+/* *
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+  public:
+    ListNode *detectCycle(ListNode *head) {
+      ListNode *meet = hasCycle(head);
+      if(!meet)
+        return meet;
+
+      ListNode *cur, *prev;
+      int loop_length = 1;
+      prev = meet;
+      cur = prev->next;
+      prev->next = (ListNode*)set_mark(prev->next);
+      while(cur!=meet) {
+        prev = cur;
+        cur = cur->next;
+        prev->next = (ListNode*)set_mark(prev->next);
+        loop_length++;
+      }
+
+      cur = head;
+      while(!is_marked(cur->next)) {
+        cur = (ListNode*)unset_mark(cur->next);
+      }
+
+      return cur;
+    }
+
+    // return the meet node
+    ListNode *hasCycle(ListNode *head) {
+      ListNode *turtle, *rabbit;
+      turtle = rabbit = head;
+
+      while(rabbit!=NULL) {
+        turtle = turtle->next;
+        rabbit = rabbit->next;
+        if(!rabbit) {// rabbit is NULL
+          return rabbit;
+        }
+        rabbit = rabbit->next;
+        if(rabbit==turtle) {// find
+          return rabbit;
+        }
+      }
+
+      return rabbit;
+    }
+
+    // is marked
+    long is_marked(void* address) {
+
+      return ((long)address & 0x1L);
+    }
+
+    // set mark
+    void* set_mark(void* address) {
+      return (void*)((long)address | 0x1L);
+    }
+
+    // unset mark
+    void* unset_mark(void* address) {
+      return (void*)((long)address & ~0x1L);
+    }
+};
